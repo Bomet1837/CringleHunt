d5bc994e7bf98433c23db07727151cbef43a82be
Bomet1837 <158185256+Bomet1837@users.noreply.github.com>
2024-06-20 21:01:48 +0100

    initial commit
    just creating the project file, adding some primitives and setting the game area

---
+++ home.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event.is_action_pressed("shoot") && Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
